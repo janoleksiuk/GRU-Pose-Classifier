e7979e8161dedad4675909ec9d882bf56c50c8e0
janoleksiuk <jan.oleksiuk01@gmail.com>
2025-09-04 23:03:49 +0200

    Trained first architecture on 4 classes

--- models/models.py
+++ models/models.py
@@ -1,7 +1,7 @@
 import torch.nn as nn
 
 class PoseGRU(nn.Module):
-    def __init__(self, input_dim, hidden_dim=128, num_layers=2, num_classes=16):
+    def __init__(self, input_dim, hidden_dim=128, num_layers=2, num_classes=4):
         super(PoseGRU, self).__init__()
         self.gru = nn.GRU(input_dim, hidden_dim, num_layers, batch_first=True)
         self.fc = nn.Linear(hidden_dim, num_classes)
